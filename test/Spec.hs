7430e25a159476683fca2117b3558a5534bc24e1
Arthur Jamet <arthur.jamet@gmail.com>
2024-08-21 20:04:53 +0200

    EDSL: Test Native Backend

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,8 @@
+import qualified InfinAdd.EDSL.Backends.NativeTest as N
 import qualified InfinAdd.Parser.ParserTest as P
 import Test.Hspec (hspec)
 
 main :: IO ()
 main = hspec $ do
     P.specs
+    N.specs
