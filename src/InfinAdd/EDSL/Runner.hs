dbada0ed714144ba0857d368b96ac3aeb1ed0018
Arthur Jamet <arthur.jamet@gmail.com>
2024-08-29 08:31:38 +0200

    C Backend: Complete Test suite for sub

--- src/InfinAdd/EDSL/Runner.hs
+++ src/InfinAdd/EDSL/Runner.hs
@@ -2,9 +2,9 @@ module InfinAdd.EDSL.Runner (run, InfinAddBackend (..)) where
 
 import Control.Monad.Free (foldFree)
 import Control.Monad.IO.Class (MonadIO)
+import InfinAdd.EDSL.Backends.C (runC)
 import InfinAdd.EDSL.Backends.Native (runNative)
 import InfinAdd.EDSL.EDSL (InfinAdd)
-import InfinAdd.EDSL.Backends.C (runC)
 
 data InfinAddBackend = Native | C deriving (Eq)
 
--- test/InfinAdd/EDSL/Backends/CTest.hs
+++ test/InfinAdd/EDSL/Backends/CTest.hs
@@ -1,15 +1,59 @@
 module InfinAdd.EDSL.Backends.CTest (specs) where
 
-import InfinAdd.EDSL.EDSL (InfinAdd, add)
+import Data.Text (Text)
+import InfinAdd (parse, toEDSL)
 import InfinAdd.EDSL.Runner (InfinAddBackend (C), run)
 import Test.Hspec
+import Text.Printf (perror, printf)
 
 specs :: Spec
 specs = describe "C Backend" $ do
-    describe "Addition (Positive Numbers)" $ do
-        it "Simple Addition" $ test (add 1 1) 2
-        it "Other Simple Addition" $ test (add 1 10) 11
-        it "Big Addition with one big number" $ test (add 1000000000000000000 1) 1000000000000000001
-        it "Big Addition with big numbers" $ test (add 122345574728797979 2938797967657517) 125284372696455496
-test :: InfinAdd Integer -> Integer -> Expectation
-test a n = run C a >>= (`shouldBe` n)
+    describe "Addition" $ do
+        describe "Positive numbers" $ do
+            itShouldBe "1 + 1" 2
+            itShouldBe "1 + 10" 11
+            itShouldBe "100000000000000000000 + 1" 100000000000000000001
+            itShouldBe "435439435845843984358439 + 5359766785665822182412343254343435433339459569" 5359766785665822182412778693779281277323818008
+        describe "Both numbers are negative" $ do
+            itShouldBe "-2 + (-3)" (-5)
+            itShouldBe "-99 + (-1)" (-100)
+            itShouldBe "-807965 + (-34532)" (-842497)
+        describe "Left operand is negative" $ do
+            itShouldBe "-24 + 1" (-23)
+        describe "Right operand is negative" $ do
+            itShouldBe "1 + (-24)" (-23)
+            itShouldBe "101 + (-99)" 2
+            itShouldBe "101 + (-999)" (-898)
+            itShouldBe "987143265 + (-876435)" 986266830
+    describe "Substraction" $ do
+        describe "Positive Numbers" $ do
+            describe "Result is positive" $ do
+                itShouldBe "3 - 2" 1
+                itShouldBe "24 - 10" 14
+                itShouldBe "100 - 100" 0
+                itShouldBe "435439435845843984358439 - 2" 435439435845843984358437
+            describe "Result is negative" $ do
+                itShouldBe "2 - 3" (-1)
+                itShouldBe "99 - 100" (-1)
+        describe "Negative Numbers" $ do
+            describe "Left operand is smaller" $ do
+                itShouldBe "(-2) - (-3)" 1
+                itShouldBe "(-10) - (-300)" 290
+                itShouldBe "(-14) - (-5)" (-9)
+                itShouldBe "(-1000) - (-1000)" 0
+            describe "Right operand is bigger" $ do
+                itShouldBe "(-1000) - (-100)" (-900)
+                itShouldBe "(-99) - (-1)" (-98)
+        describe "Left operand is negative" $ do
+            itShouldBe "(-24) - 1" (-25)
+            itShouldBe "(-807965) - 10" (-807975)
+        describe "Right operand is negative" $ do
+            itShouldBe "1 - (-24)" 25
+            itShouldBe "10 - (-10)" 20
+            itShouldBe "10 - (-99)" 109
+
+itShouldBe :: Text -> Integer -> SpecWith ()
+itShouldBe s n = it (printf "%s == %d" s n) $ do
+    case parse s of
+        Left err -> perror err
+        Right expr -> run C (toEDSL expr) >>= (`shouldBe` n)
