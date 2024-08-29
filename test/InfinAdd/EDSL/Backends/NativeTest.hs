b4092846b0ea3dcfbcd952c2ec922c5b93f17040
Arthur Jamet <arthur.jamet@gmail.com>
2024-08-29 20:44:43 +0200

    EDSL: substract -> sub

--- test/InfinAdd/EDSL/Backends/NativeTest.hs
+++ test/InfinAdd/EDSL/Backends/NativeTest.hs
@@ -6,10 +6,10 @@ import Test.Hspec
 specs :: Spec
 specs = describe "Native Backend" $ do
     it "Simple Addition" $ test (add 1 1) 2
-    it "Simple Substration" $ test (substract 10 1) 9
-    it "Addition and substraction" $ flip test (-1) $ do
+    it "Simple Substration" $ test (sub 10 1) 9
+    it "Addition and subion" $ flip test (-1) $ do
         two <- add 1 1
-        substract two 3
+        sub two 3
 
 test :: InfinAdd Integer -> Integer -> Expectation
 test a n = run Native a >>= (`shouldBe` n)
